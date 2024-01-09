254933d1a05c3cdc055f6a7b3973a2112830f386
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-01-09 18:09:39 +0100

    Update register_view.php

---
+++ views/register_view.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body class="max-w-screen-lg mx-auto">
+    <header class="flex items-center justify-between py-2">
+        <a href="index.php" class="px-2 lg:px-0 font-bold capitalize">
+            WIKI
+        </a>
+        <ul class="hidden md:inline-flex items-center">
+            <li class="px-2 md:px-4">
+                <a href="/index.php" class="text-red-800 font-semibold hover:text-blue-600"> Home </a>
+            </li>
+            <li class="px-2 md:px-4">
+                <a href="#" class="text-gray-500 font-semibold hover:text-blue-600"> About </a>
+            </li>
+            <li class="px-2 md:px-4">
+                <a href="#" class="text-gray-500 font-semibold hover:text-blue-600"> Press </a>
+            </li>
+            <li class="px-2 md:px-4">
+                <a href="#" class="text-gray-500 font-semibold hover:text-blue-600"> Contact </a>
+            </li>
+
+            <?php if (isset($_SESSION["login"])) {
+
+                if (empty($_SESSION["admin"])) {
+            ?>
+                    <li class="px-2 md:px-4 hidden md:block">
+                        <a id="manage-wikis-btn" class="cursor-pointer text-gray-500 font-semibold hover:text-blue-600"> Manage My Wikis </a>
+                    </li>
+                <?php } else { ?>
+                    <li class="px-2 md:px-4 hidden md:block">
+                        <a id="manage-wikis-btn" class="cursor-pointer text-gray-500 font-semibold hover:text-blue-600"> Manage Wikis </a>
+                    </li>
+                <?php } ?>
+                <li class="px-2 md:px-4 hidden md:block">
+                    <form action="index.php?page=home" method="post">
+                        <button name="logout" class="text-gray-500 font-semibold hover:text-green-600"> Logout</button>
+                    </form>
+                </li>
+            <?php } else { ?>
+                <li class="px-2 md:px-4 hidden md:block">
+                    <a href="login_view.php" class="text-gray-500 font-semibold hover:text-blue-600"> Login </a>
+                </li>
+                <li class="px-2 md:px-4 hidden md:block">
+                    <a href="register_view.php" class="text-gray-500 font-semibold hover:text-blue-600">
+                        Register </a>
+                </li>
+
+            <?php } ?>
+        </ul>
+
+    </header>
+    <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Create and account
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                            <input name="first_name" id="first_name" class="bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-withe dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="First Name">
+                        </div>
+                        <div>
+                            <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                            <input name="last_name" id="last_name" class="bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-withe dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Last Name">
+                        </div>
+                        <div>
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input name="email" id="email" class="bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-withe dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required="">
+                        </div>
+                        <div>
+                            <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                        </div>
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                            </div>
+                            <div class="ml-3 text-sm">
+                                <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                            </div>
+                        </div>
+                        <button type="submit" class="w-full bg-blue-600 text-white hover:bg-withe-700 text-blue focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create Account
+                        </button>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            Already have an account? <a href="login_view.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
