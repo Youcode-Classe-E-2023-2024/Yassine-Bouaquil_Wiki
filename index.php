9b3c3a9636a139e98fcd164eb4ab80d7acf615f0
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-01-09 11:45:07 +0100

    index.php navbar

--- index.php
+++ index.php
@@ -12,6 +12,48 @@
     <a href="index.php" class="px-2 lg:px-0 font-bold capitalize">
         WIKI
     </a>
+    <ul class="hidden md:inline-flex items-center">
+        <li class="px-2 md:px-4">
+            <a href="index.php" class="text-red-800 font-semibold hover:text-blue-600"> Home </a>
+        </li>
+        <li class="px-2 md:px-4">
+            <a href="#" class="text-gray-500 font-semibold hover:text-blue-600"> About </a>
+        </li>
+        <li class="px-2 md:px-4">
+            <a href="#" class="text-gray-500 font-semibold hover:text-blue-600"> Press </a>
+        </li>
+        <li class="px-2 md:px-4">
+            <a href="#" class="text-gray-500 font-semibold hover:text-blue-600"> Contact </a>
+        </li>
+
+        <?php if (isset($_SESSION["login"])) {
+
+            if (empty($_SESSION["admin"])) {
+                ?>
+                <li class="px-2 md:px-4 hidden md:block">
+                    <a id="manage-wikis-btn" class="cursor-pointer text-gray-500 font-semibold hover:text-blue-600"> Manage My Wikis </a>
+                </li>
+            <?php } else { ?>
+                <li class="px-2 md:px-4 hidden md:block">
+                    <a id="manage-wikis-btn" class="cursor-pointer text-gray-500 font-semibold hover:text-blue-600"> Manage Wikis </a>
+                </li>
+            <?php } ?>
+            <li class="px-2 md:px-4 hidden md:block">
+                <form action="index.php?page=home" method="post">
+                    <button name="logout" class="text-gray-500 font-semibold hover:text-green-600"> Logout</button>
+                </form>
+            </li>
+        <?php } else { ?>
+            <li class="px-2 md:px-4 hidden md:block">
+                <a href="index.php?page=login" class="text-gray-500 font-semibold hover:text-blue-600"> Login </a>
+            </li>
+            <li class="px-2 md:px-4 hidden md:block">
+                <a href="index.php?page=register" class="text-gray-500 font-semibold hover:text-blue-600">
+                    Register </a>
+            </li>
+
+        <?php } ?>
+    </ul>
 
     </header>
 </body>
