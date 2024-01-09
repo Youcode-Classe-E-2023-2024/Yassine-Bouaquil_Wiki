1e9d4deabb2592ac67d6e32f42586050769f3f1c
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-01-09 18:09:50 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body class="max-w-screen-lg mx-auto">
     <header class="flex items-center justify-between py-2">
-    <a href="index.php" class="px-2 lg:px-0 font-bold capitalize">
-        WIKI
-    </a>
+        <a href="index.php" class="px-2 lg:px-0 font-bold capitalize">
+            WIKI
+        </a>
     </header>
-
-    
 </body>
 </html>
