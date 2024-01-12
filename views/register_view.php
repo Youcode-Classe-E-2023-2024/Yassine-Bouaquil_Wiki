55f16bbd410a42d63a15b6b019c52ac7b4b40d8f
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-01-12 18:17:37 +0100

    Update register_view.php

--- views/register_view.php
+++ views/register_view.php
@@ -1,6 +1,24 @@
 <?php
+require_once '../includes/config_session.inc.php';
+require_once '../models/dashboard_model.php';
+require_once '../includes/dbh.inc.php';
+require_once 'login_view.inc.php';
 include_once '../includes/header.php';
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 
 <body>
 </head>
